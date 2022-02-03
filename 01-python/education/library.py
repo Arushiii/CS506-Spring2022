6f70f4156039ff596a1ad0d5c91e55af538e5ff7
Arushi Shrivastava <arushishrivastava1997@gmail.com>
2022-02-02 21:00:39 -0500

    file that is calling library function

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,3 +1,27 @@
+import PySimpleGUI as sg
+import os
+import tkinter
+import io
+import os
+from PIL import Image, ImageDraw
 def draw_library():
-    print("library not found")
-    return
+    #print("library not found")
+    #return
+    print(os.getcwd())
+    img = Image.open(os.getcwd() + "\\library.jpg")
+    img.thumbnail((1200, 800))
+    bio = io.BytesIO()
+    img.save(bio, format="PNG")
+
+    layout = [[[sg.Text("School:", font= ("Arial", 25))], 
+    [sg.Image(data=bio.getvalue())]],
+    [sg.Button("Don't Show Me Anymore")]]
+
+    window = sg.Window("Image", layout, size=(1200, 800))
+#closing the window:
+    while True:
+        event, values = window.read()
+        if event == "Don't Show Me Anymore" or event == sg.WIN_CLOSED:
+            break
+
+draw_library()
