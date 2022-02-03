bd4a8ec57f06d0377841857d06269f840b44fffc
Arushi Shrivastava <arushishrivastava1997@gmail.com>
2022-02-02 21:01:40 -0500

    file that is calling school function

--- 01-python/education/school.py
+++ 01-python/education/school.py
@@ -1,26 +1,27 @@
-from PIL import Image, ImageDraw
 import PySimpleGUI as sg
 import os
 import tkinter
 import io
-
+import os
+from PIL import Image, ImageDraw
 def draw_school():
     #from lab's example
-    img = Image.open("BUschool.jpg")
+    print(os.getcwd())
+    img = Image.open(os.getcwd() + "\\BUschool.jpg")
     img.thumbnail((1200, 800))
     bio = io.BytesIO()
     img.save(bio, format="PNG")
 
     layout = [[[sg.Text("School:", font= ("Arial", 25))], 
-    [sg.Image(Data=bio.getvalue())]],
+    [sg.Image(data=bio.getvalue())]],
     [sg.Button("Don't Show Me Anymore")]]
 
     window = sg.Window("Image", layout, size=(1200, 800))
-
+#closing the window:
     while True:
         event, values = window.read()
         if event == "Don't Show Me Anymore" or event == sg.WIN_CLOSED:
             break
+#draw_school()
 
-    window.close()
-    return
+#Nathan Vollertsen, zhitao gan, Jason Chow, Peng Huang, Ji Zhao
